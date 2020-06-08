70cc2f82afc5de04498b059304c5ff124db0dea4
Matt Stroud <lixingke06@gmail.com>
2020-06-07 22:05:43 -0700

    create forum controller, add index route

---
+++ StackUnderflow/Controllers/ForumsController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using StackUnderflow.Models;
+
+namespace StackUnderflow.Controllers
+{
+  public class ForumsController : Controller
+  {
+    private readonly StackUnderflowContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public ForumsController(UserManager<ApplicationUser> userManager, StackUnderflowContext db)
+    {
+      _db = db;
+      _userManager = userManager;
+    }
+
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
