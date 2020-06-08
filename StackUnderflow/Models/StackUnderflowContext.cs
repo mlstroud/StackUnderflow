e1403bca21c15eea4d55ad9d70cc671ac0a7ab2e
Matt Stroud <lixingke06@gmail.com>
2020-06-07 22:26:00 -0700

    add DbSets to context

--- StackUnderflow/Models/StackUnderflowContext.cs
+++ StackUnderflow/Models/StackUnderflowContext.cs
@@ -5,7 +5,9 @@ namespace StackUnderflow.Models
 {
   public class StackUnderflowContext : IdentityDbContext<ApplicationUser>
   {
-
+    public DbSet<Forum> Forums { get; set; }
+    public virtual DbSet<Thread> Threads { get; set; }
+    public virtual DbSet<Post> Posts { get; set; }
     public StackUnderflowContext(DbContextOptions options) : base(options) { }
   }
 }
