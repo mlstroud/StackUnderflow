dc1631fb871d819b5404c9840054032b73da961e
Matt Stroud <lixingke06@gmail.com>
2020-06-08 23:17:19 -0700

    add forum ID to post model

--- StackUnderflow/Migrations/20200609060034_PostUpdate.Designer.cs
+++ StackUnderflow/Migrations/20200609060034_PostUpdate.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StackUnderflow.Models;
 
 namespace StackUnderflow.Migrations
 {
     [DbContext(typeof(StackUnderflowContext))]
-    partial class StackUnderflowContextModelSnapshot : ModelSnapshot
+    [Migration("20200609060034_PostUpdate")]
+    partial class PostUpdate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ StackUnderflow/Migrations/20200609060034_PostUpdate.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StackUnderflow.Migrations
+{
+    public partial class PostUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ForumId",
+                table: "Posts",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ForumId",
+                table: "Posts");
+        }
+    }
+}
