9d1475c62b603181de879444e0cde346821b208e
Matt Stroud <lixingke06@gmail.com>
2020-06-08 22:59:20 -0700

    add ForumId

--- StackUnderflow/Models/Post.cs
+++ StackUnderflow/Models/Post.cs
@@ -7,6 +7,7 @@ namespace StackUnderflow.Models
     public int PostId { get; set; }
     public string Message { get; set; }
     public int ThreadId { get; set; }
+    public int ForumId {get; set;}
     public virtual ApplicationUser User { get; set; }
   }
 }
